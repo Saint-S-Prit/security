0c94e2a874860b8ec7872ab560a83167b9ce1a00
Saint-S-Prit <mamadoulaminediop93@gmail.com>
2023-04-20 15:48:48 +0000

    test commit

--- src/main/java/com/espritech/security/user/UserServiceImpl.java
+++ src/main/java/com/espritech/security/user/UserServiceImpl.java
@@ -24,7 +24,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto save(UserDto userDto) {
-        System.out.println("test");
+        System.out.println("test 2");
         List<String> errors = UserValidator.validate(userDto);
         if (!errors.isEmpty()) {
             log.error("User is not valid {}", userDto);
